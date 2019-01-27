972eca3f95ce3bb3f91952474259689ea332df93
SpiderMurphy <vincenzocaleca@gmail.com>
2019-01-27 21:39:54 +0100

    Added character's detail view unit test

---
+++ app/src/test/java/cyan/com/nwnadmin/characters/details/CharacterDetailPresenterImplTest.kt
@@ -0,0 +1,44 @@
+package cyan.com.nwnadmin.characters.details
+
+import cyan.com.nwnadmin.model.data.PlayerCharacter
+import io.mockk.CapturingSlot
+import io.mockk.MockKAnnotations
+import io.mockk.every
+import io.mockk.impl.annotations.RelaxedMockK
+import io.mockk.verify
+import org.junit.Before
+import org.junit.Test
+
+import org.junit.Assert.*
+
+class CharacterDetailPresenterImplTest {
+    @RelaxedMockK
+    private lateinit var view: CharacterDetailView
+    private lateinit var presenter: CharacterDetailPresenter
+
+    @Before
+    fun setUp() {
+        MockKAnnotations.init(this)
+        val character = PlayerCharacter(
+            1,
+            "testusername",
+            "testcdkey",
+            "testname",
+            0,
+            15)
+
+        presenter = CharacterDetailPresenterImpl(view, character)
+    }
+
+    @Test
+    fun viewReady() {
+        val slot = CapturingSlot<PlayerCharacter>()
+        presenter.viewReady()
+        verify { view.onDisplayCharacter(capture(slot)) }
+        assert(slot.captured.username == "testusername")
+    }
+
+    @Test
+    fun destroy() {
+    }
+}
